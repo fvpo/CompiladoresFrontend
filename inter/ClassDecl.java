5dff7e4902edb2b108fb36a04012d6d92f5efcc7
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2025-10-29 11:06:33 -0300

    Add files via upload

---
+++ inter/ClassDecl.java
@@ -0,0 +1,21 @@
+package inter;
+import symbols.Env;
+
+import java.util.List;
+
+public class ClassDecl extends Stmt {
+    public final String name;
+    public final String superName;   // null se não houver extends
+    public final List<Stmt> members;
+
+    public ClassDecl(String name, String superName, List<Stmt> members) {
+        this.name = name;
+        this.superName = superName;
+        this.members = members;
+    }
+
+    @Override
+    public void exec(Env env) {
+        env.put(name, this);
+    }
+}
