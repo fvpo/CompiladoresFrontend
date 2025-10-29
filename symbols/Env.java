2b731a0d2a87edda445030d5f0074376df60f48b
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2025-10-28 23:01:19 -0300

    Add files via upload

---
+++ symbols/Env.java
@@ -0,0 +1,44 @@
+package symbols;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Env {
+    private static final Map<String, Object> table = new HashMap<>();
+    private Env prev;
+
+    public Env(Env n) {
+        prev = n;
+    }
+
+    public static void put(String name, Object val) {
+        table.put(name, val);
+    }
+
+    public static Object getValue(String name) {
+        return table.get(name);
+    }
+
+    public void setValue(String name, Object value) {
+        if (table.containsKey(name)) {
+            table.put(name, value);
+        } else if (prev != null) {
+            prev.setValue(name, value);
+        } else {
+            throw new RuntimeException("Variável não declarada: " + name);
+        }
+    }
+
+    public static Object get(String name) {
+        Object found = table.get(name);
+        if (found != null) {
+            return found;
+        } else {
+            return null; // não encontrado
+        }
+    }
+
+    public static boolean containsInCurrent(String name) {
+        return table.containsKey(name);
+    }
+}
---
+++ symbols/Type.java
@@ -0,0 +1,36 @@
+package symbols;
+
+import lexer.*;
+
+public class Type extends Word{
+    public int width = 0;
+
+    public Type(String s, Tag tag, int w){
+        super(s, tag);
+        width = w;
+    }
+
+    public Type(String s, Tag tag){
+        super(s, tag);
+        width = -1;
+    }
+
+    public static final Type
+            voidWord    = new Type("void", Tag.BASIC, 0),
+            intWord     = new Type("int", Tag.BASIC, 4),
+            boolWord     = new Type("bool", Tag.BASIC, 1),
+            floatWord   = new Type("float", Tag.BASIC, 8),
+            charWord = new Type("char", Tag.BASIC, 1),
+            stringWord  = new Type("string", Tag.STRING);
+
+    public static boolean numeric(Type p) {
+        return (p == Type.charWord || p == Type.intWord || p == Type.floatWord);
+    }
+
+    public static Type max(Type p1, Type p2) {
+        if (!numeric(p1) || !numeric(p2)) return null;
+        else if (p1 == Type.floatWord || p2 == Type.floatWord) return Type.floatWord;
+        else if (p1 == Type.intWord   || p2 == Type.intWord)   return Type.intWord;
+        else return Type.charWord;
+    }
+}
